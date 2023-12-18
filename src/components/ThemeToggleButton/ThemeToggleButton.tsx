8e4dbe6245ff0d5f191151fb11d93f5915ba0780
Maxime AUMONT <maxime.aumont@reseau.eseo.fr>
2023-12-18 08:58:02 +0100

    Button Switch light/dark mode

--- src/components/ThemeToggleButton/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton/ThemeToggleButton.tsx
@@ -1,14 +1,24 @@
-import { themeStore } from '../ThemeStore/ThemeStore.tsx'; 
+import { observer } from 'mobx-react-lite';
+import { themeStore } from '../ThemeStore/ThemeStore';
+import './ThemeToggleButton.css';
 
 const ThemeToggleButton = () => {
+  const isDarkMode = themeStore.theme === 'dark';
+
+  const toggleTheme = () => {
+    themeStore.toggleTheme();
+  };
+
   return (
     <button 
-      className="theme-toggle-button" 
-      onClick={() => themeStore.toggleTheme()}
+      className={`theme-toggle-button ${isDarkMode ? 'dark-mode' : 'light-mode'}`}
+      onClick={toggleTheme}
     >
-      {themeStore.theme === 'light' ? 'Dark Mode' : 'Light Mode'}
+      <span className="toggle-icon sun">🌞</span>
+      <div className={`slider ${isDarkMode ? 'dark-mode' : 'light-mode'}`}></div>
+      <span className="toggle-icon moon">🌜</span>
     </button>
   );
 };
 
-export default ThemeToggleButton;
+export default observer(ThemeToggleButton);
